ac174fc774a8ba791144c4d54b08f337130fb7d0
unknown <unknown@example.com>
2020-03-26 21:39:55 +0800

    Initial Commit

---
+++ pages/me/me.js
@@ -0,0 +1,17 @@
+//me.js
+const app = getApp()
+
+Page({
+  sethead:function(){
+
+  },
+  bindKeyInput1:function(e){
+
+  },
+  bindKeyInput2:function(e){
+
+  },
+  bindKeyInput3:function(e){
+    
+  }
+})
